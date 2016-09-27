3e51114dcddf3fc89106d2205f0aa2eae32ab24e
Maroof Ahmad <ahmadmaroof8@gmail.com>
2016-09-28 01:48:44 +0530

    code size reduced

--- linear-regression-gradient-descent.py
+++ linear-regression-gradient-descent.py
@@ -13,41 +13,30 @@ df.insert(0,"THETA",1)
 y = np.matrix(df["MEDV"])
 X = np.matrix(df.drop("MEDV",axis=1))
 
-# X = np.matrix([[1,-5,2],[1,7,3],[1,11,-2],[1,15,-4]])
-# y = np.array([2,3,4,5])
-
 def predict(X,ps):
-    hp = 0
-    for i in range(X.shape[0]):
-        print X[i],ps[i]
-        hp = hp + X[i]*ps[i]
-    return hp
+    x = X[np.newaxis]
+    return np.dot(X,ps)
 
-def calc(X,params,y,i):
-    r = np.dot(X,params.T) - y
-    r = r*X[:,i]
-    return r
-
-def gradient_descent(X,y,pms,a):
-    m,n = X.shape
-    new_params = np.zeros(n)
-    for i in range(n):
-        x = (a/m)*calc(X,pms,y,i)
-        new_params[i] = pms[i] - x
-    return new_params
 
 def linregr(X,y,a):
     m,n = X.shape
+    y = y[np.newaxis]
+    y = y.T
     params = np.zeros(n)
-    for i in range(100):
-        params = gradient_descent(X,y,params,a)
+    params = params[np.newaxis]
+    params = params.T
+    for i in range(1000):
+        h = np.dot(X,params)
+        h = h - y
+        g = np.dot(X.T,h)/m
+        params = params - a*g
     return params
 
 a=0.000005
 ps = linregr(X,y,a)
-# print ps
+print ps
 
 X1 = np.array([1,0.9,0.00,8.5,0,0.6,6,90,5,4,311,22,400,17])
 
 hp = predict(X1,ps)
-print hp
+print hp[0][0]
